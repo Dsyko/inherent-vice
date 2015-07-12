097077a7db00fc45d47528ba090d779919898041
David Sykora <dsykora@gmail.com>
2015-07-11 19:49:26 -0700

    Better splash screen

--- meteor/mobile-config.js
+++ meteor/mobile-config.js
@@ -21,20 +21,11 @@ App.launchScreens({
   'iphone': 'resources/splash/Default~iphone.png',
   'iphone_2x': 'resources/splash/Default@2x~iphone.png',
   'iphone5': 'resources/splash/Default-568h@2x.png',
-  'ipad_portrait': 'resources/splash/splash-768x1024.png',
-  'ipad_portrait_2x': 'resources/splash/splash-768x1024@2x.png',
-  'ipad_landscape': 'resources/splash/splash-1024x768.png',
-  'ipad_landscape_2x': 'resources/splash/splash-1024x768@2x.png',
+  'ipad_portrait': 'resources/splash/Default-Portrait~ipad.png',
+  'ipad_portrait_2x': 'resources/splash/Default-Portrait@2x~ipad.png',
+  'ipad_landscape': 'resources/splash/Default-Landscape~ipad.png',
+  'ipad_landscape_2x': 'resources/splash/Default-Landscape@2x~ipad.png',
 
-  // Android
-  'android_ldpi_portrait': 'resources/splash/splash-200x320.png',
-  'android_ldpi_landscape': 'resources/splash/splash-320x200.png',
-  'android_mdpi_portrait': 'resources/splash/splash-320x480.png',
-  'android_mdpi_landscape': 'resources/splash/splash-480x320.png',
-  'android_hdpi_portrait': 'resources/splash/splash-480x800.png',
-  'android_hdpi_landscape': 'resources/splash/splash-800x480.png',
-  'android_xhdpi_portrait': 'resources/splash/splash-720x1280.png',
-  'android_xhdpi_landscape': 'resources/splash/splash-1280x720.png'
 });
 
 App.setPreference('StatusBarOverlaysWebView', 'false');
